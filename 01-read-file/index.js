1dd2124692807d25e5ec5c297d9f1fcbf5cefb74
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-11-02 23:52:54 +0400

    feat: implements read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+const readStream = fs.createReadStream(__dirname+'\\text.txt',{
+    encoding:'utf8',
+});
+
+
+
+readStream.on('readable', () => {
+
+    let data;
+    while (data = readStream.read()){
+        console.log(data);
+    }
+    
+});
