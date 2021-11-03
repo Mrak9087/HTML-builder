ffee4d20948c7e71e10572c7a3590ca43c1202a9
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-11-03 23:48:28 +0400

    feat: implement 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const {readFile, writeFile, readdir} = require('fs/promises');
+const path = require('path');
+
+const buildCss = async (srcDir, destDir) => {
+    const destFile = path.join(destDir,'bundle.css');
+
+    const cssFiles = await readdir(srcDir,{withFileTypes: true});
+    // console.log(cssFiles);
+    let data = '';
+    for (let file of cssFiles){
+        if (file.isFile()){
+            if (path.extname(file.name).slice(1) === 'css'){
+                data += await readFile(path.join(srcDir,file.name));
+            }
+        }
+    }
+    await writeFile(destFile, data);
+}
+
+buildCss(path.join(__dirname,'styles'),path.join(__dirname,'project-dist'));
