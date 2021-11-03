bd93e50ac6fc9c02ec19928d82c19623c347c406
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-11-03 13:45:17 +0400

    feat: implement 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = require('process');
+const readln = require('readline');
+
+const writeStream = fs.createWriteStream(path.join(__dirname,'text.txt'),{
+    encoding:'utf8',
+});
+const rl = readln.createInterface({ input: stdin, output:  stdout});
+
+stdout.write('Enter text. For close enter "exit" or press CTRL+C\n');
+
+const closeScript = () =>{
+    writeStream.end();
+    rl.close();
+}
+
+rl.on('line', (data) =>{
+    if (data === 'exit'){
+        closeScript();
+    } else {
+        writeStream.write(`${data}\n`);
+    }
+})
+
+rl.on('SIGINT', () => {
+    closeScript();
+});
