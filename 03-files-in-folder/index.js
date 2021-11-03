1deb5b0d98be76eee4205142fe9efbcd8a91e592
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-11-03 18:18:04 +0400

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+const { readdir,stat } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+const  showFilesInformation = async (pathDir) => {
+    
+    try {
+        const files = await readdir(pathDir,{withFileTypes: true});
+        for (const file of files){
+            if (file.isDirectory()){
+                await showFilesInformation(path.join(pathDir,file.name))
+            }else{
+                const fileInfo = await stat(path.join(pathDir,file.name));
+                const ext = path.extname(file.name);
+                const flName = path.basename(file.name,ext);
+                const fileSize = fileInfo.size;
+                console.log(`${flName} - ${ext} - ${fileSize/1000}kb`);
+            }
+            
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+const pathDir = path.join(__dirname,'secret-folder');
+
+showFilesInformation(pathDir);
+
+
