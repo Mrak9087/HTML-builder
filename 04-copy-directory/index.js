8f9bec014695a4d221722aff959f707dd7bb4302
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-11-03 22:47:24 +0400

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const {mkdir, rmdir, readdir, copyFile} = require('fs/promises');
 
+
+const recreateDir = async (path) => {
+    await rmdir(path,{recursive:true});
+    await mkdir(path,{recursive:true});
+}
+
+const copyDir = async (srcDir, destDir) => {
+    await recreateDir(destDir);
+
+    const contentDir = await readdir(srcDir, {withFileTypes: true});
+
+    for (let file of contentDir){
+        if (file.isDirectory()){
+            await copyDir(path.join(srcDir,file.name),path.join(destDir,file.name));
+        } else {
+            await copyFile(path.join(srcDir,file.name),path.join(destDir,file.name))
+        }
+    }
+}
+
+copyDir(path.join(__dirname,'files'),path.join(__dirname,'files-copy'));
